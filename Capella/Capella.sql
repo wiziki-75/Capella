fa9cd802d0635633bff54cee891e67398286d259
Anthony Guerrand <59973442+wiziki-75@users.noreply.github.com>
2025-02-04 16:36:38 +0100

    Update Capella.sql

--- Capella/Capella.sql
+++ Capella/Capella.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : ven. 03 jan. 2025 à 15:09
--- Version du serveur : 5.7.33
--- Version de PHP : 7.4.19
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 04 fév. 2025 à 16:34
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,11 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cap_activity_log` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `activity_type` varchar(50) NOT NULL,
   `details` text NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- --------------------------------------------------------
 
@@ -41,10 +42,11 @@ CREATE TABLE `cap_activity_log` (
 --
 
 CREATE TABLE `cap_like` (
-  `id_post` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL
+  `id_post` int NOT NULL,
+  `user_id` int NOT NULL,
+  `post_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 --
 -- Déclencheurs `cap_like`
@@ -78,10 +80,10 @@ DELIMITER ;
 --
 
 CREATE TABLE `cap_post` (
-  `id_post` int(11) NOT NULL,
+  `id_post` int NOT NULL,
   `contenu` text NOT NULL,
-  `post_id` int(11) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
+  `post_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
   `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -98,6 +100,38 @@ DELIMITER $$
 CREATE TRIGGER `after_post_insert` AFTER INSERT ON `cap_post` FOR EACH ROW BEGIN
     INSERT INTO cap_activity_log (activity_type, details)
     VALUES ('Post Created', CONCAT('Post ID: ', NEW.id_post, ', User ID: ', NEW.user_id));
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cap_subscription`
+--
+
+CREATE TABLE `cap_subscription` (
+  `id` int NOT NULL,
+  `subscriber_id` int NOT NULL,
+  `subscribed_to_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Déclencheurs `cap_subscription`
+--
+DELIMITER $$
+CREATE TRIGGER `after_subscription_delete` AFTER DELETE ON `cap_subscription` FOR EACH ROW BEGIN
+    INSERT INTO cap_activity_log (activity_type, details)
+    VALUES ('Subscription Removed', CONCAT('Subscriber ID: ', OLD.subscriber_id, ', Subscribed To ID: ', OLD.subscribed_to_id));
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `after_subscription_insert` AFTER INSERT ON `cap_subscription` FOR EACH ROW BEGIN
+    INSERT INTO cap_activity_log (activity_type, details)
+    VALUES ('Subscription Added', CONCAT('Subscriber ID: ', NEW.subscriber_id, ', Subscribed To ID: ', NEW.subscribed_to_id));
 END
 $$
 DELIMITER ;
@@ -129,6 +163,15 @@ ALTER TABLE `cap_post`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Index pour la table `cap_subscription`
+--
+ALTER TABLE `cap_subscription`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `subscriber_to_unique` (`subscriber_id`,`subscribed_to_id`),
+  ADD KEY `subscriber_id` (`subscriber_id`),
+  ADD KEY `subscribed_to_id` (`subscribed_to_id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -136,19 +179,25 @@ ALTER TABLE `cap_post`
 -- AUTO_INCREMENT pour la table `cap_activity_log`
 --
 ALTER TABLE `cap_activity_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT pour la table `cap_like`
 --
 ALTER TABLE `cap_like`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_post` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `cap_post`
 --
 ALTER TABLE `cap_post`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_post` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `cap_subscription`
+--
+ALTER TABLE `cap_subscription`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Contraintes pour les tables déchargées
@@ -167,6 +216,13 @@ ALTER TABLE `cap_like`
 ALTER TABLE `cap_post`
   ADD CONSTRAINT `cap_post_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `cap_post` (`id_post`) ON DELETE CASCADE,
   ADD CONSTRAINT `cap_post_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `cap_subscription`
+--
+ALTER TABLE `cap_subscription`
+  ADD CONSTRAINT `cap_subscription_ibfk_1` FOREIGN KEY (`subscriber_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cap_subscription_ibfk_2` FOREIGN KEY (`subscribed_to_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
